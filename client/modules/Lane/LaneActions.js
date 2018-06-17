0bf9d7707cfb986591283c3f61f8ab82cce99d59
Jakub Kilar <kuba.kilar@gmail.com>
2018-06-17 19:55:32 +0200

    add create lanes and fetchlanes actions

--- client/modules/Lane/LaneActions.js
+++ client/modules/Lane/LaneActions.js
@@ -1,10 +1,12 @@
 import uuid from 'uuid';
+import callApi from '../../util/apiCaller';
 
 // Export Constants
 export const CREATE_LANE = 'CREATE_LANE';
 export const UPDATE_LANE = 'UPDATE_LANE';
 export const DELETE_LANE = 'DELETE_LANE';
 export const EDIT_LANE = 'EDIT_LANE';
+export const CREATE_LANES = 'CREATE_LANES';
 
 // Export Actions
 export function createLane(lane) {
@@ -36,5 +38,20 @@ export function editLane(laneId) {
   return {
     type: EDIT_LANE,
     laneId,
+  };
+}
+
+export function createLanes(lanesData) {
+  return {
+    type: CREATE_LANES,
+    lanes: lanesData,
+  };
+}
+
+export function fetchLanes() {
+  return (dispatch) => {
+    return callApi('lanes').then(res => {
+      dispatch(createLanes(res.lanes));
+    });
   };
 }
