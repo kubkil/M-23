544277a6c8d38cf3e54925f3e3e59974f5a911bf
Jakub Kilar <kuba.kilar@gmail.com>
2018-06-17 16:21:09 +0200

    add route for edit note

--- server/routes/note.routes.js
+++ server/routes/note.routes.js
@@ -9,4 +9,7 @@ router.route('/notes').post(NoteController.addNote);
 // Delete a note by noteId
 router.route('/notes/:noteId').delete(NoteController.deleteNote);
 
+// Edit a note by noteId
+router.route('/notes/:noteId').put(NoteController.editNote);
+
 export default router;
