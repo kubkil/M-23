e892a35b60a40a8d20bf4743fb87a12264325dea
Jakub Kilar <kuba.kilar@gmail.com>
2018-06-17 16:02:09 +0200

    add route for edit lane name

--- server/routes/lane.routes.js
+++ server/routes/lane.routes.js
@@ -12,4 +12,7 @@ router.route('/lanes').post(LaneController.addLane);
 // Delete a lane by laneId
 router.route('/lanes/:laneId').delete(LaneController.deleteLane);
 
+// Edit a lane name by laneId
+router.route('/lanes/:laneId').put(LaneController.editLaneName);
+
 export default router;
