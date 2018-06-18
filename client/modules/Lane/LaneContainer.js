d12bb7bad1e6148edaa0e64dd2efa273a516e65e
Jakub Kilar <kuba.kilar@gmail.com>
2018-06-18 19:11:37 +0200

    refactor mapStateToProps

--- client/modules/Lane/LaneContainer.js
+++ client/modules/Lane/LaneContainer.js
@@ -3,11 +3,9 @@ import Lane from './Lane';
 import * as laneActions from './LaneActions';
 import { createNote } from '../Note/NoteActions';
 
-const mapStateToProps = (state, ownProps) => {
-  return {
-    laneNotes: ownProps.lane.notes.map(noteId => state.notes.find(note => note.id === noteId))
-  };
-};
+const mapStateToProps = (state, ownProps) => ({
+  laneNotes: ownProps.lane.notes.map(noteId => state.notes[noteId])
+});
 
 const mapDispatchToProps = {
   ...laneActions,
