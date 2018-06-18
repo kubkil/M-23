40846b5fda6b2fddf2dd2b6bc3ad7550c4a09a68
Jakub Kilar <kuba.kilar@gmail.com>
2018-06-18 19:06:07 +0200

    remove uuid

--- client/modules/Lane/LaneActions.js
+++ client/modules/Lane/LaneActions.js
@@ -1,4 +1,3 @@
-import uuid from 'uuid';
 import callApi from '../../util/apiCaller';
 import { lanes } from '../../util/schema';
 import { normalize } from 'normalizr';
@@ -16,7 +15,6 @@ export function createLane(lane) {
   return {
     type: CREATE_LANE,
     lane: {
-      id: uuid(),
       notes: [],
       ...lane,
     }
