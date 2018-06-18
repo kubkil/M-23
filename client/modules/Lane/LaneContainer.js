c6d8755d5ff87b237b0fdd03fa62ac7dfcacebc7
Jakub Kilar <kuba.kilar@gmail.com>
2018-06-18 19:26:35 +0200

    add new imports and dispatch them to props

--- client/modules/Lane/LaneContainer.js
+++ client/modules/Lane/LaneContainer.js
@@ -1,17 +1,17 @@
 import { connect } from 'react-redux';
 import Lane from './Lane';
-import * as laneActions from './LaneActions';
-// import { createLaneRequest, fetchLanes } from '../Lane/LaneActions';
-import { createNote } from '../Note/NoteActions';
+import { deleteLane, updateLane, editLane } from './LaneActions';
+import { createNoteRequest } from '../Note/NoteActions';
 
 const mapStateToProps = (state, ownProps) => ({
   laneNotes: ownProps.lane.notes.map(noteId => state.notes[noteId])
 });
 
 const mapDispatchToProps = {
-  ...laneActions,
-  addNote: createNote,
-  createLane: createLaneRequest,
+  editLane,
+  deleteLane,
+  updateLane,
+  addNote: createNoteRequest,
 };
 
 export default connect(
