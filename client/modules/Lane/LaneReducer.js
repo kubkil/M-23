16cf78ca00a7d4e7e61ce39058cc1a5751f01842
Jakub Kilar <kuba.kilar@gmail.com>
2018-06-19 21:58:49 +0200

    refactor

--- client/modules/Lane/LaneReducer.js
+++ client/modules/Lane/LaneReducer.js
@@ -10,27 +10,33 @@ export default function lanes(state = initialState, action) {
     case CREATE_LANE:
     case UPDATE_LANE:
       return { ...state, [action.lane.id]: action.lane };
+
     case EDIT_LANE: {
       const lane = { ...state[action.id], editing: true };
       return { ...state, [action.id]: lane };
     }
+
     case CREATE_LANES:
       return { ...action.lanes };
+
     case DELETE_NOTE: {
       const newLane = { ...state[action.laneId] };
       newLane.notes = newLane.notes.filter(noteId => noteId !== action.noteId);
 
       return { ...state, [action.laneId]: newLane };
     }
+
     case CREATE_NOTE: {
       const newLane = { ...state[action.laneId] };
       newLane.notes = newLane.notes.concat(action.note.id);
 
       return { ...state, [action.laneId]: newLane };
     }
+
     case DELETE_LANE: {
       return omit(state, action.laneId);
     }
+
     default:
       return state;
   }
